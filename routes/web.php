f026e7f3b583a1a8028ccd1c7aab5d52bb507032
ALU-syntax <ardianiqbal40@gmail.com>
2024-03-16 17:38:44 +0700

    allow super admin untuk mengakses semua modul

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,9 @@ Route::middleware('auth')->group(function () {
         Route::resource('roles', RoleController::class);
         Route::resource('permissions', PermissionController::class);
         Route::get('akses-role/{role}/role', [AksesRoleController::class, 'getPermissionsByRole']);
-        Route::resource('akses-role', AksesRoleController::class)->except(['create', 'store', 'destroy'])->parameters(['akses-role'=> 'role']);
+        Route::resource('akses-role', AksesRoleController::class)->except(['create', 'store', 'delete'])->parameters(['akses-role'=> 'role']);
         Route::get('akses-user/{user}/user', [AksesUserController::class, 'getPermissionsByUser']);
-        Route::resource('akses-user', AksesUserController::class)->except(['create', 'store', 'destroy'])->parameters(['akses-user'=> 'user'])->middleware('can:read konfigurasi/akses-user');
+        Route::resource('akses-user', AksesUserController::class)->except(['create', 'store', 'delete'])->parameters(['akses-user'=> 'user'])->middleware('can:read konfigurasi/akses-user');
     });
 });
 
